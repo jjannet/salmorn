c945fc128be1b860187bf2652c5e15912cdd5541
jirawat_off@hotmail.co.th <jirawat_off@hotmail.co.th>
2018-01-18 10:35:32 +0700

    .

---
+++ GenModelString/GenModelString/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace GenModelString
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInput = new System.Windows.Forms.RichTextBox();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.tbOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(12, 41);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(530, 491);
+            this.tbInput.TabIndex = 0;
+            this.tbInput.Text = "";
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(12, 12);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(75, 23);
+            this.btnGen.TabIndex = 1;
+            this.btnGen.Text = "Generate";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(548, 41);
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(530, 491);
+            this.tbOutput.TabIndex = 0;
+            this.tbOutput.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1092, 547);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.tbInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox tbInput;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.RichTextBox tbOutput;
+    }
+}
+
---
+++ GenModelString/GenModelString/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenModelString
+{
+    public partial class Form1 : Form
+    {
+        private List<string> numbetStrs = new List<string>() { "int", "float", "long", "decimal" };
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            string input = tbInput.Text;
+            StringBuilder output = new StringBuilder();
+
+
+            foreach(var l in input.Split('\n'))
+            {
+                
+                string str = l.Replace("public ", "").Replace(" { get; set; }", "").Trim();
+                string type = str.Split(' ')[0];
+                string name = str.Split(' ')[1];
+
+                if (isNumber(type.ToLower())) output.Append(name + ": number;\n");
+                else if (type.ToLower().Contains("string")) output.Append(name + ": string;\n");
+                else if(type.ToLower().Contains("date")) output.Append(name + ": Date;\n");
+                else if (type.ToLower().Contains("bool")) output.Append(name + ": Boolean;\n");
+            }
+
+            tbOutput.Text = output.ToString();
+        }
+
+        private bool isNumber(string type)
+        {
+            foreach (var n in numbetStrs)
+            {
+                if (type.Contains(n)) return true;
+            }
+            return false;
+        }
+    }
+}
